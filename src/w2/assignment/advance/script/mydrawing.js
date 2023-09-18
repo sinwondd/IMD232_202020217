a47b7e593a6444b7969e9ca9a2b070f197f2b0f6
kkimsin <127167530+kkimsin@users.noreply.github.com>
2023-09-18 20:51:58 +0900

    3주차

--- src/w2/assignment/advance/script/mydrawing.js
+++ src/w2/assignment/advance/script/mydrawing.js
@@ -1,24 +1,24 @@
 function setup() {
   setCanvasContainer('canvasYO', 600, 300, true);
-  console.log(width);
-  console.log(height);
+  // console.log(width);
+  // console.log(height);
 }
 
 function draw() {
   background('#FAF0E6');
 
-  fill('#FFE4C4');
+  fill('#D2B48C');
   rect(0, 0, width, 20);
 
-  fill('#D2B48C');
-  rect(0, height - 100, width, 100);
+  fill('#8B4513');
+  rect(0, height * 0.8, width * 100, height * 0.9);
 
   stroke('#DEB887');
-  strokeWeight(8);
+  strokeWeight(17);
   fill('#87CEFA');
   circle(width / 2, height / 2, width / 5);
-  line(width / 2, height / 5, width / 2);
-  line(width / 2, height / 1.44, 60);
+  line(width * 0.4, height / 2, width * 0.6, height / 2);
+  line(width * 0.5, height * 0.7, width * 0.5, height * 0.3);
 
   noStroke();
   fill('#FFF8DC');
@@ -29,40 +29,39 @@ function draw() {
 
   noStroke();
   fill('#FFFF00');
-  circle(width / 2, height * 0.2, width * 0.09);
+  circle(width / 2, height * 0.2, width * 0.07);
   fill('white');
-  circle(width / 2, height * 0.16, width * 0.04);
-
-  fill('#DEB887');
-  rect(360, 300, 10, -50);
-  rect(360, 250, 15, -60);
-  rect(320, 300, 10, -50);
-  rect(320, 300, 10, -50);
-  rect(310, 260, 60, -20);
+  circle(width / 2, height * 0.16, width * 0.03);
 
   fill('#CD853F');
-  rect(310, 260, 60, -8);
+  // scale(1);
+  rect(width / 3, height * 0.7, width / 60, height * 0.2, 5);
+  rect(width / 2.5, height * 0.7, width / 60, height * 0.2, 5);
+  rect(width / 3.2, height * 0.68, width / 9, height * 0.05, 20);
+  rect(width / 2.5, height * 0.5, width / 45, height * 0.2, 20);
+  fill('#A0522D');
+  rect(width / 3.2, height * 0.71, width / 9, height * 0.02, 10);
+
   fill('#DEB887');
-  rect(120, 300, 15, -90);
+  rect(width / 12, height * 0.6, width / 60, height * 0.3, 5);
+  rect(width / 2.8, height * 0.6, width / 60, height * 0.3, 5);
   fill('#DEB887');
-  rect(285, 300, 15, -90);
-  fill('#DEB887');
-  rect(100, 200, 220, 10);
+  rect(width / 14, height / 1.7, width / 3.2, height / 30, 5);
   fill('#CD853F');
-  rect(110, 210, 200, 10);
+  rect(width / 14, height / 1.62, width / 3.2, height / 60, 5);
   fill('#FFFFFF');
-  rect(215, 200, 20, -15);
-  circle(225, 175, 45, -20);
+
+  rect(width / 5.45, height / 1.76, width / 30, height / 50);
+  circle(width / 5, height * 0.51, width / 15);
 
   fill('#800000');
-  rect(460, 300, 20, -50, 15);
-  rect(660, 300, 20, -50, 15);
+  rect(width / 1.6, height * 0.7, width / 40, height * 0.2, 10);
+  rect(width / 1.04, height * 0.7, width / 40, height * 0.2, 10);
   fill('#808000');
-  rect(460, 180, 230, 85, 20);
-  fill('#6B8E23');
-  rect(460, 185, 230, 85, 20);
-  fill('#556B2F');
-  rect(450, 230, 250, 55, 20);
+  rect(width / 1.635, height / 1.5, width / 2.6, height / 6, 20);
   fill('#228B22');
-  rect(450, 230, 250, 15, 20);
+  rect(width / 1.6, height / 1.8, width / 2.8, height / 6, 20);
+
+  fill('#6B8E23');
+  rect(width / 1.635, height / 1.5, width / 2.6, height / 15, 20);
 }
