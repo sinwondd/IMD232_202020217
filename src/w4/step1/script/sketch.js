61a3b3793a15480f32ea1c32e62be1e45f3f38c3
kkimsin <127167530+kkimsin@users.noreply.github.com>
2023-10-17 22:23:48 +0900

    중간과제

---
+++ src/w4/step1/script/sketch.js
@@ -0,0 +1,48 @@
+let bodies = [];
+let G = 0.05;
+let showVector = false;
+
+function setup() {
+  setCanvasContainer('canvas', 1, 1, true);
+  reset();
+}
+
+function draw() {
+  background(255);
+
+  for (let i = 0; i < bodies.length; i++) {
+    for (let j = 0; j < bodies.length; j++) {
+      if (i !== j) {
+        let forceForJ = bodies[i].attract(bodies[j]);
+        bodies[j].applyForce(forceForJ);
+      }
+    }
+    bodies[i].update();
+    bodies[i].display();
+    if (showVector) {
+      bodies[i].displayVectors();
+    }
+  }
+}
+
+function mousePressed() {
+  if (isMouseInsideCanvas()) {
+    reset();
+  }
+}
+
+function reset() {
+  bodies = [];
+  let numBodies = 20;
+  for (let i = 0; i < numBodies; i++) {
+    let radius = random(20, 50);
+    let mass = pow(radius, 2) / 100;
+    bodies.push(new Body(random(width), random(height), mass, radius));
+  }
+}
+
+function keyPressed() {
+  if (key === 's' || key === 'S') {
+    showVector = !showVector;
+  }
+}
