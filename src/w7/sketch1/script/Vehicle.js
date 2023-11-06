785211d4f2c580fc745c80ff6fa410069601c7c0
kkimsin <127167530+kkimsin@users.noreply.github.com>
2023-11-06 13:28:53 +0900

    기말

--- src/w7/sketch1/script/Vehicle.js
+++ src/w7/sketch1/script/Vehicle.js
@@ -1,17 +1,12 @@
 class Vehicle {
   constructor(x, y, mass, rad, speedMx, forceMx) {
     this.pos = createVector(x, y);
-    this.vel = createVector();
-    this.acc = createVector();
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
     this.mass = mass;
     this.rad = rad;
     this.speedMx = speedMx;
     this.forceMx = forceMx;
-  }
-
-  applyForce(force) {
-    const acc = p5.Vector.div(force, this.mass);
-    this.acc.add(acc);
   }
 
   update() {
@@ -21,30 +16,30 @@ class Vehicle {
     this.acc.mult(0);
   }
 
+  applyForce(force) {
+    const acc = p5.Vector.div(force, this.mass);
+    this.acc.add(acc);
+  }
+
   display() {
-    // const headingAngle = atan2(this.vel.y, this.vel.x);
-    const headingAngle = this.vel.heading();
+    const angle = this.vel.heading();
+    fill(127);
+    stroke(0);
     push();
     translate(this.pos.x, this.pos.y);
-    rotate(headingAngle);
-    fill(0);
-    noStroke();
+    rotate(angle);
     beginShape();
-    vertex(this.rad, 0);
-    vertex(this.rad * cos(radians(-135)), this.rad * sin(radians(-135)));
-    vertex(0, 0);
-    vertex(this.rad * cos(radians(135)), this.rad * sin(radians(135)));
+    vertex(this.rad * 2, 0);
+    vertex(-this.rad * 2, -this.rad);
+    vertex(-this.rad * 2, this.rad);
     endShape(CLOSE);
-    noFill();
-    stroke('red');
-    ellipse(0, 0, 2 * this.rad);
     pop();
   }
 
   seek(target) {
-    const desiredVelocity = p5.Vector.sub(target, this.pos);
-    desiredVelocity.setMag(this.speedMx);
-    const steer = p5.Vector.sub(desiredVelocity, this.vel);
+    const steer = p5.Vector.sub(target, this.pos);
+    steer.setMag(this.speedMx);
+    steer.sub(this.vel);
     steer.limit(this.forceMx);
     this.applyForce(steer);
   }
--- src/w7/sketch1/script/seekingTarget.js
+++ src/w7/sketch1/script/seekingTarget.js
@@ -3,18 +3,21 @@ let mVec;
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
-
-  vehicle = new Vehicle(width / 2, height / 2, 1, 20, 10, 0.1);
+  vehicle = new Vehicle(width / 2, height / 2, 1, 6, 8, 0.2);
   mVec = createVector();
-
-  background(255);
 }
 
 function draw() {
+  background(255);
+
   mVec.set(mouseX, mouseY);
+
+  fill(127);
+  stroke(0);
+  strokeWeight(2);
+  circle(mVec.x, mVec.y, 48);
 
   vehicle.seek(mVec);
   vehicle.update();
-  background(255);
   vehicle.display();
 }
