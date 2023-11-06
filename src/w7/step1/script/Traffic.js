d4ff067b2228163d4600c3bfeaf15fdf78a69e37
kkimsin <127167530+kkimsin@users.noreply.github.com>
2023-11-06 14:02:03 +0900

    끛

--- src/w7/step1/script/Traffic.js
+++ src/w7/step1/script/Traffic.js
@@ -11,23 +11,31 @@ class Traffic {
       //   현재 "Vehicle" 객체를 다른 모든 "Vehicle" 객체와 분리시키는 힘을 계산하고 "separate" 변수에 저장
       separate.mult(1);
       eachVehicle.applyForce(separate);
+      //   "separate" 힘을 현재 "Vehicle" 객체에 적용
       const align = eachVehicle.align(this.vehicles);
       //   현재 "Vehicle" 객체를 다른 모든 "Vehicle" 객체와 정렬시키는 힘을 계산하고 "align" 변수에 저장
       align.mult(0.5);
       eachVehicle.applyForce(align);
+      //   "align" 힘을 현재 "Vehicle" 객체에 적용
       const cohesion = eachVehicle.cohesion(this.vehicles);
+      //   "Vehicle" 객체를 다른 모든 "Vehicle" 객체와 함께 모이게 하는 힘을 계산하고 "cohesion" 변수에 저장시킴
       cohesion.mult(0.5);
       eachVehicle.applyForce(cohesion);
+      //   : "cohesion" 힘을 현재 "Vehicle" 객체에 적용
       eachVehicle.update();
+      //   \현재 "Vehicle" 객체의 위치 및 속도를 업데이트
       eachVehicle.borderInfinite();
+      //   현재 "Vehicle" 객체가 화면 경계를 넘어갈 때 반대편으로 이동
       eachVehicle.display();
     });
   }
 
   addVehicle(x, y) {
     const mass = 1;
+    // "Vehicle" 객체의 질량을 설정
     this.vehicles.push(
       new Vehicle(x, y, mass, mass * 12, 5, 0.1, color(random(360), 100, 40))
+      //   "vehicles" 배열에 새로운 "Vehicle" 객체를 추가. 이 객체는 주어진 위치 (x, y)와 설정된 매개변수를 사용하여 생성
     );
   }
 }
--- src/w7/step1/script/sketch.js
+++ src/w7/step1/script/sketch.js
@@ -1,12 +1,17 @@
 let traffic;
 let infiniteOffset = 80;
+// 화면 경계를 넘어갈 때 개체를 반대편으로 이동시키는 데 사용
 
 function setup() {
   setCanvasContainer('canvas', 3, 2, true);
   colorMode(HSL, 360, 100, 100, 100);
+  //   컬러 모드
   background('white');
+  //   화면의 배경색을 흰색
   traffic = new Traffic();
+  //   "Traffic" 클래스의 인스턴스를 생성하고 "traffic" 변수에 할당
   for (let n = 0; n < 10; n++) {
+    // 반복문을 사용하여 초기에 10개의 "Vehicle" 객체를 임의의 위치에 생성하고 "Traffic" 객체에 추가
     traffic.addVehicle(random(width), random(height));
   }
 }
@@ -14,8 +19,10 @@ function setup() {
 function draw() {
   background('white');
   traffic.run();
+  //   "Traffic" 객체의 "run" 메서드를 호출하여 개체 그룹의 모든 "Vehicle" 객체를 업데이트하고 표시
 }
 
 function mouseDragged() {
   traffic.addVehicle(mouseX, mouseY);
+  //   마우스를 드래그할 때 호출되는 함수로, 마우스의 현재 위치에 새 "Vehicle" 객체를 추가하여 마우스 위치에 개체를 생성
 }
